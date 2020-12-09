2cb6306840463b14a493004bba0742c9f75807f4
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-08 21:41:44 -0600

    geolocation start

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './App.css';
 import Signup from './components/Signup/Signup'
+import Geoloc from './components/Geolocation/Geolocation'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
+
       <Signup />
+      <Geoloc />
+      
     </div>
   );
 }
