91e0afb17c025e1919c707bf033638c53f4bcb6d
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-09 21:05:52 -0600

    updating app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import './App.css';
-import Signup from './components/Signup/Signup'
-import Geoloc from './components/Geolocation/Geolocation'
+import Jumbotron from './components/Jumbotron';
+import Nav from './components/Nav';
+//import Signup from './pages/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-
-      <Signup />
-      <Geoloc />
-      
+    <div>
+        <Nav />
+        <Jumbotron />
     </div>
   );
 }
