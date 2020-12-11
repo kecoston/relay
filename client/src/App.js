23f0f25389d237a1cca0b212bb04e9120d25a769
Zack White <zhwhite18@yahoo.com>
2020-12-10 19:37:06 -0600

    installed react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react'
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Jumbotron from './components/Jumbotron';
 import Nav from './components/Nav';
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import Signup from './components/Signup/Signup'
-=======
-//import Signup from './pages/Signup'
->>>>>>> dd54183e3e5775a98023fa8a7089fd61ee19b6ba
-=======
-
-
->>>>>>> a7b68c6ef126d3dced49b3ea2d6db85c3db6701c
+import Signup from './components/Signup/Signup'
 
 function App() {
   return (
     <div>
         <Nav />
         <Jumbotron />
-
-
-
+        <Signup />
     </div>
   );
 }
