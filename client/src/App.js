eb594a5317e1dd4f243a6924dad8e8f0c2e184e7
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-10 20:35:55 -0600

    geolocation getting coordinates

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,14 @@ import React from 'react'
 import './App.css';
 import Jumbotron from './components/Jumbotron';
 import Nav from './components/Nav';
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import Signup from './components/Signup/Signup'
-=======
-//import Signup from './pages/Signup'
->>>>>>> dd54183e3e5775a98023fa8a7089fd61ee19b6ba
-=======
-
-
->>>>>>> a7b68c6ef126d3dced49b3ea2d6db85c3db6701c
+import Geolocate from "./components/Geolocation/Geolocation"
 
 function App() {
   return (
     <div>
         <Nav />
         <Jumbotron />
-
-
-
+        <Geolocate />
     </div>
   );
 }
--- client/src/components/Geolocation/Geolocation.js
+++ client/src/components/Geolocation/Geolocation.js
@@ -1,46 +1,52 @@
-import React from "react";
-import { geolocated } from "react-geolocated";
- 
-class Demo extends React.Component {
+import React from 'react'
+
+
+class Geolocate extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            userAddress: null
+        };
+        this.getLocation = this.getLocation.bind(this);
+        this.getCoordinates = this.getCoordinates.bind(this)
+    }
+
+    getLocation() {
+        console.log("click working")
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.getCoordinates);
+        } else {
+            alert("Geolocation is not supported by this browswer")
+        }
+    }
+
+    getCoordinates(position) {
+        console.log(position)
+        this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }
+
     render() {
-        return !this.props.isGeolocationAvailable ? (
-            <div>Please check your device settings and browswer to allow access to your location.</div>
-        ) : !this.props.isGeolocationEnabled ? (
-            <div>Geolocation is not enabled</div>
-        ) : this.props.coords ? (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>latitude</td>
-                        <td>{this.props.coords.latitude}</td>
-                    </tr>
-                    <tr>
-                        <td>longitude</td>
-                        <td>{this.props.coords.longitude}</td>
-                    </tr>
-                    <tr>
-                        <td>altitude</td>
-                        <td>{this.props.coords.altitude}</td>
-                    </tr>
-                    <tr>
-                        <td>heading</td>
-                        <td>{this.props.coords.heading}</td>
-                    </tr>
-                    <tr>
-                        <td>speed</td>
-                        <td>{this.props.coords.speed}</td>
-                    </tr>
-                </tbody>
-            </table>
-        ) : (
-            <div>Getting the location data&hellip; </div>
-        );
+        return (
+            <div className="Geo">
+                <h2>Geolocation Example</h2>
+
+                <button onClick={this.getLocation}>Get Coordinates</button>
+                <h4>HTML5 Coordinates</h4>
+                <ul>
+                    <li>Latitude: {this.state.latitude} </li>
+                    <li>Longitude: {this.state.longitude} </li>
+                </ul>
+                <h4> Google Maps Reverse Geocoding</h4>
+                <p>Address: {this.state.userAddress}</p>
+            </div>
+        )
     }
 }
- 
-export default geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 5000,
-})(Demo);
+
+export default Geolocate
