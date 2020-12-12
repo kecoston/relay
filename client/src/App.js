91d36a745675ea49b86b8c87e34dc1bb73fb8726
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-12 09:53:10 -0600

    geolocation map

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Jumbotron from './components/Jumbotron';
 import Nav from './components/Nav';
 import Geolocate from "./components/Geolocation/Geolocation"
 
+console.log(process.env.REACT_APP_GOOGLE_API)
 function App() {
   return (
     <div>
