865008bc73ddfdb2cfbef111abbe37b62397c489
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-15 17:46:01 -0600

    setting up activity and contact cards to connect models

---
+++ client/src/components/Contact/Contact.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import MoodIcon from "@material-ui/icons/Mood";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  }
+}));
+
+export default function Contact() {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <MoodIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="First Name Last Name" secondary="000 000 0000" />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+    </List>
+  );
+}
