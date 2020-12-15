cf8879afe5a9cbc014abc9cddd641ea6d5387a3b
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-15 17:07:28 -0600

    added timer component

--- client/src/components/Timer/index.js
+++ client/src/components/Timer/index.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import TimerDisplay from "../TimerDisplay"
+import TimerBtn from "../TimerBtn"
+import "./timer.css"
 
-function Timer() {
+
+function Timer () {
+
+    const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+    const [interv, setInterv] = useState();
+    const [status, setStatus] = useState(0);
+    // Not started = 0
+    // started = 1
+    // stopped = 2
+  
+    const start = () => {
+      run();
+      setStatus(1);
+      setInterv(setInterval(run, 10));
+    };
+  
+    var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+  
+    const run = () => {
+      if(updatedM === 60){
+        updatedH++;
+        updatedM = 0;
+      }
+      if(updatedS === 60){
+        updatedM++;
+        updatedS = 0;
+      }
+      if(updatedMs === 100){
+        updatedS++;
+        updatedMs = 0;
+      }
+      updatedMs++;
+      return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+    };
+  
+    const pause = () => {
+      clearInterval(interv);
+      setStatus(2);
+    };
+  
+    const reset = () => {
+      clearInterval(interv);
+      setStatus(0);
+      setTime({ms:0, s:0, m:0, h:0})
+    };
+  
+    const resume = () => start();
+  
+  
     return (
-        <div>
-            <h1>(Timer)</h1>
-        </div>
+      <div className="main-section">
+       <div className="clock-holder">
+            <div className="stopwatch">
+                 <TimerDisplay time={time}/>
+                 <TimerBtn status={status} resume={resume} reset={reset} pause={pause} start={start}/>
+            </div>
+       </div>
+      </div>
     );
-}
-
-export default Timer;
+  }
+  
+  export default Timer;
+  
---
+++ client/src/components/TimerBtn/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function TimerBtn(props) {
+  return (
+    <div>
+      {(props.status === 0)? 
+        <button className="stopwatch-btn stopwatch-btn-gre"
+        onClick={props.start}>Start</button> : ""
+      }
+
+      {(props.status === 1)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-red"
+                  onClick={props.pause}>Pause</button>
+          <button className="stopwatch-btn stopwatch-btn-yel"
+                  onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
+
+     {(props.status === 2)? 
+        <div>
+          <button className="stopwatch-btn stopwatch-btn-gre"
+                  onClick={props.resume}>Resume</button>
+          <button className="stopwatch-btn stopwatch-btn-yel"
+                  onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
+     
+    </div>
+  );
+}
+
+export default TimerBtn;
