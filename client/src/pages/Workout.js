530d9f561162f1988e74a8d74ad9d24c2de54b02
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-17 19:08:39 -0600

    workout button form fixed

--- client/src/pages/Workout.js
+++ client/src/pages/Workout.js
@@ -3,6 +3,32 @@ import Jumbotron from "../components/Jumbotron";
 import { Row, Col, Container } from "../components/Grid";
 import TimerBtn from "../components/TimerBtn";
 import Timer from "../components/Timer";
+import API from "../utils/API";
+
+
+
+// useEffect () {
+
+// }
+
+
+// function getWorkoutDetails () {
+//     API.getActivities()
+//     .then(res => 
+//         setActivities()
+//         .catch(res => 
+//             set))
+// }
+ 
+
+// function intervalEvent () {
+
+//     setTimeout(myFunction, 3000)
+//     console.log("message sent")
+
+
+// }
+
 
 export default class Workout extends Component {
     render() {
