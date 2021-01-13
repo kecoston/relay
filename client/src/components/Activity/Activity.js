81fc1e4e97cc04cf040f2baca1ced62f88ece7fb
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2021-01-13 08:58:56 -0600

    updating for presentation

--- client/src/components/Activity/Activity.js
+++ client/src/components/Activity/Activity.js
@@ -12,7 +12,6 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import moment from 'moment'
-import API from "../../utils/API";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -46,19 +45,11 @@ export default function Activity({
   title,
   date,
   interval,
-  contacts,
+  contactId
 }) {
 
-useEffect((contacts) => {
-  API.getContact(contacts)
-  .then(res => 
-    getContact(res.data) 
-  ).catch((err) => console.log(err))
-
-}, []) 
-
   const classes = useStyles();
-  const [contact, getContact] = React.useState()
+
   const [expanded, setExpanded] = React.useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
@@ -90,7 +81,7 @@ useEffect((contacts) => {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            On {moment(date).format('MMM Do YYYY, h:mm:ss a')} you went for a insert time run and set {contact.firstName} as
+            On {moment(date).format('MMM Do YYYY, h:mm:ss a')} you went for a insert time run and set {contactId} as
             your emergency contact. You had messages sent to them every {interval}.
           </Typography>
         </AccordionDetails>
